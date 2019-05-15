b255c6fc23615251de35e1273e2e66096563f5c3
Justin SB <justinsb@google.com>
2019-05-15 07:55:19 -0700

    Copy the bootstrap-vz repo from our fork, instead of cloning it on the target

--- imagebuilder/pkg/imagebuilder/builder.go
+++ imagebuilder/pkg/imagebuilder/builder.go
@@ -3,9 +3,11 @@ package imagebuilder
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"path"
+	"path/filepath"
 
 	"k8s.io/kube-deploy/imagebuilder/pkg/imagebuilder/executor"
 )
@@ -48,11 +50,14 @@ func (b *Builder) BuildImage(template []byte, extraEnv map[string]string, logdir
 		return err
 	}
 
-	//err = ssh.Exec("git clone https://github.com/andsens/bootstrap-vz.git " + tmpdir + "/bootstrap-vz")
-	err = b.target.Exec("git", "clone", b.config.BootstrapVZRepo, "-b", b.config.BootstrapVZBranch, tmpdir+"/bootstrap-vz")
-	if err != nil {
+	if err := b.copyTree("forks/bootstrap-vz", tmpdir+"/bootstrap-vz"); err != nil {
 		return err
 	}
+
+	//err = b.target.Exec("git", "clone", b.config.BootstrapVZRepo, "-b", b.config.BootstrapVZBranch, tmpdir+"/bootstrap-vz")
+	//if err != nil {
+	//	return err
+	//}
 
 	err = b.target.Put(tmpdir+"/template.yml", len(template), bytes.NewReader(template), 0644)
 	if err != nil {
@@ -68,6 +73,42 @@ func (b *Builder) BuildImage(template []byte, extraEnv map[string]string, logdir
 	err = cmd.Run()
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// copyTree performs a fairly inefficient upload of our bootstrap-vz fork
+// We don't expect a lot of files and don't expect any to be particularly big
+func (b *Builder) copyTree(srcDir string, destDir string) error {
+	files, err := ioutil.ListFiles(srcDir)
+	if err != nil {
+		return fmt.Errorrf("error listing files in %q: %v", srcDir, err)
+	}
+
+	for _, f := range files {
+		srcChild := filepath.Join(srcDir, f.Name())
+		destChild := path.Join(destDir, f.Name())
+
+		if f.IsDir() {
+			if err := b.target.Mkdir(destChild, f.Mode()); err != nil {
+				return fmt.Errorf("error creating destination directory %q: %v", destChild, err)
+			}
+
+			if err := b.copyTree(srcChild, destChild); err != nil {
+				return err
+			}
+		} else {
+			// We don't expect any of these files to be big
+			b, err := ioutil.ReadFile(srcChild)
+			if err != nil {
+				return fmt.Errorf("error reading %q: %v", srcChild, err)
+			}
+
+			if err := b.target.Put(destChild, len(b), bytes.NewReader(b), f.Mode()); err != nil {
+				return fmt.Errorf("error writing %q: %v", destChild, err)
+			}
+		}
 	}
 
 	return nil
